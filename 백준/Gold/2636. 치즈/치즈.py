426b62ffade5dd05e701494820da2a7a7bdb63c5
zxcvb <156629458+ooyejinn@users.noreply.github.com>
2024-06-09 19:24:32 +0900

    [Gold IV] Title: 치즈, Time: 144 ms, Memory: 114180 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.py
+++ 백준/Gold/2636. 치즈/치즈.py
@@ -15,48 +15,55 @@ cnt += 1 한 뒤 Q 진행
 while Q:
     범위 내 도달 가능한 1이 있다면 2로 만든다
     범위 내 도달 가능한 1이 없다면 해당 Q 종료 (0, 2밖에 없는 경우)
-    
+
 만약 1이 남아있지 않다면
     cnt, 2의 개수 출력
 
 1이 남아있다면
     2를 0으로 바꾼 뒤 Q 진행
 '''
-from copy import deepcopy
+# from copy import deepcopy
 from collections import deque
 
 dr = [1, -1, 0, 0]
 dc = [0, 0, 1, -1]
 
+
 def melting(now_arr):
     Q = deque([(0, 0)])
-    visited = [[0]*M for _ in range(N)]
+    visited = [[0] * M for _ in range(N)]
+    melt_lst = []
+    # 변경점 1. 녹일 치즈 위치 저장. 이걸 추가하고 deepcopy를 뺐다
 
     while Q:
         r, c = Q.popleft()
 
         for d in range(4):
-            nr, nc = r+dr[d], c+dc[d]
-            if 0<=nr<N and 0<=nc<M and visited[nr][nc] == 0:
+            nr, nc = r + dr[d], c + dc[d]
+            if 0 <= nr < N and 0 <= nc < M and not visited[nr][nc]:
                 if now_arr[nr][nc] == 0:
                     visited[nr][nc] = 1
                     Q.append((nr, nc))
                 elif now_arr[nr][nc] == 1:
                     visited[nr][nc] = 1
-                    now_arr[nr][nc] = 2
+                    # now_arr[nr][nc] = 2
+                    # 변경점 2
+                    melt_lst.append((nr, nc))
 
-    return now_arr
+    # 변경점 3. 한번에 치즈 녹이기
+    for r, c in melt_lst:
+        now_arr[r][c] = 0
 
 
 def count(now_arr):
-    return sum(row.count(1) for row in arr)
+    return sum(row.count(1) for row in now_arr)
+
 
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
 
 time = 0
 last = 0
-
 
 while True:
 
@@ -66,14 +73,17 @@ while True:
         break
 
     last = cnt
-    arr = melting(deepcopy(arr))
-
-    for r in range(N):
-        for c in range(M):
-            if arr[r][c] == 2:
-                arr[r][c] = 0
+    melting(arr)
 
     time += 1
+
+    # 변경점 4. 2 -> 0으로 변경시키는 과정을 melting함수 안에 넣었다
+    # arr = melting(deepcopy(arr))
+    # for r in range(N):
+    #     for c in range(M):
+    #         if arr[r][c] == 2:
+    #             arr[r][c] = 0
+
 
 print(time)
 print(last)
