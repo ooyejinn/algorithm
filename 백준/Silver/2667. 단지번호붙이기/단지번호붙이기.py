8b77be52b2d05333e7be2084aed5f69270710f42
ooyejinn <156629458+ooyejinn@users.noreply.github.com>
2025-01-14 01:04:11 +0900

    [Silver I] Title: 단지번호붙이기, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,35 +1,33 @@
 dr = [1, 0, -1, 0]
 dc = [0, 1, 0, -1]
 
-def bfs(r, c):
+def bfs(sr, sc):
     Q = []
 
-    Q.append((r, c))
-    visit[r][c] = 1
+    Q.append((sr, sc))
+    visit[sr][sc] = 1
     cnt = 1
 
     while Q:
-        i, j = Q.pop(0)
+        r, c = Q.pop(0)
 
         for k in range(4):
-            ni = dr[k] + i
-            nj = dc[k] + j
-            if 0 <= ni < N and 0 <= nj < N and visit[ni][nj] == 0 and mp[ni][nj] == 1:
-                visit[ni][nj] = 1
-                Q.append((ni, nj))
+            nr, nc = dr[k]+r, dc[k]+c
+            if 0 <= nr < N and 0 <= nc < N and visit[nr][nc] == 0 and arr[nr][nc] == 1:
+                visit[nr][nc] = 1
+                Q.append((nr, nc))
                 cnt += 1
 
     return cnt
 
-
 N = int(input())
-mp = [list(map(int, input())) for i in range(N)]
+arr = [list(map(int, input())) for i in range(N)]
 visit = [[0]*N for _ in range(N)]
 result = []
 
 for r in range(N):
     for c in range(N):
-        if mp[r][c] == 1 and visit[r][c] == 0:
+        if arr[r][c] == 1 and visit[r][c] == 0:
             result.append(bfs(r, c))
 
 result.sort()
