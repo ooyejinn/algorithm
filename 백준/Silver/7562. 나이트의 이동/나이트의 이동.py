b08eae3a6b49b5c5d9e6df28ad21750cff765245
ooyejinn <156629458+ooyejinn@users.noreply.github.com>
2025-01-14 00:13:41 +0900

    [Silver I] Title: 나이트의 이동, Time: 360 ms, Memory: 137292 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -6,22 +6,23 @@ def bfs(sr, sc):
     visited[sr][sc] = 1
 
     while Q:
-        r, c, move = Q.popleft()
+        r, c, cnt = Q.popleft()
 
         if r == er and c == ec:
-            return move
+            return cnt
 
         for d in [(-1,-2), (-2,-1), (-2,1), (-1,2), (1,-2), (2,-1), (2,1), (1,2)]:
             nr, nc = r+d[0], c+d[1]
-            if 0<=nr<N and 0<=nc<N and visited[nr][nc] == -1:
+            if 0 <= nr < N and 0 <= nc < N and visited[nr][nc] == -1:
                 visited[nr][nc] = 1
-                Q.append((nr, nc, move+1))
-
+                Q.append((nr, nc, cnt + 1))
 
 T = int(input())
-for tc in range(1, T+1):
+
+for tc in range(T):
     N = int(input())
+
     sr, sc = map(int, input().split())
     er, ec = map(int, input().split())
 
-    print (bfs(sr, sc))
+    print(bfs(sr, sc))
