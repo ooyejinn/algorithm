9a24ec75f2de39af24767556271feea7ea1640ff
ooyejinn <156629458+ooyejinn@users.noreply.github.com>
2025-01-25 18:27:21 +0900

    [Gold IV] Title: 빙산, Time: 2920 ms, Memory: 35028 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -13,67 +13,74 @@ arr = [list(map(int, input().split())) for _ in range(N)]
 
 result = 0
 
-def melting():
-    Q = deque()
-    # 녹을 예정인 장소 미리 저장해서 한번에 녹이기
-    melting = [[0]*M for _ in range(N)]
 
+def melting():
+    # 빙산 개수 카운트
+    ice_cnt = 0
+    # 녹을 위치 및 양 저장
+    melting = [[0] * M for _ in range(N)]
+    # 빙산 위치 저장
+    ice_positions = []
+
+    # 빙산 위치 저장 + 녹을 양 개산
     for r in range(N):
         for c in range(M):
             if arr[r][c] > 0:
-                Q.append((r, c))
+                ice_cnt += 1
+                ice_positions.append((r, c))
+                for d in range(4):
+                    nr, nc = r+dr[d], c+dc[d]
+                    if 0 <= nr < N and 0 <= nc < M and arr[nr][nc] == 0:
+                        melting[r][c] += 1
+                        
+    for r, c in ice_positions:
+        arr[r][c] = max(0, arr[r][c] - melting[r][c])
+        if arr[r][c] == 0:
+            ice_cnt -= 1
+
+    return ice_cnt
+
+
+def check_split(ice_cnt):
+    if ice_cnt == 0:
+        return False
+
+    visited = [[0]*M for _ in range(N)]
+
+    # (r, c) for r in range(N) for c in range(M)
+    # 2차원 배열의 모든 위치(r, c) 생성하는 제너레이터 표현식
+    # if arr[r][c] > 0: 조건
+    # next(...): 제너레이터에서 조건 만족하는 첫 항목 반환
+    sr, sc = next((r, c) for r in range(N) for c in range(M) if arr[r][c] > 0)
+
+    Q = deque([(sr, sc)])
+    visited[sr][sc] = 1
+    cnt = 1
 
     while Q:
         cr, cc = Q.popleft()
-        water_cnt = 0
         for d in range(4):
             nr, nc = cr+dr[d], cc+dc[d]
-            if 0 <= nr < N and 0 <= nc < M and arr[nr][nc] == 0:
-                water_cnt += 1
-        melting[cr][cc] = water_cnt
+            if 0<=nr<N and 0<=nc<M and visited[nr][nc]==0 and arr[nr][nc]>0:
+                visited[nr][nc] = 1
+                Q.append((nr, nc))
+                cnt += 1
 
-    for r in range(N):
-        for c in range(M):
-            arr[r][c] = max(0, arr[r][c] - melting[r][c])
+    # 이렇게 한 빙산(연결된) 개수가 전체 빙산 개개의 갯수랑 다르면 return
+    return cnt != ice_cnt
 
-
-def check_split():
-    visited = [[0]*M for _ in range(N)]
-    components = 0
-
-    # 연결된거 다 확인
-    def bfs(sr, sc):
-        Q = deque([(sr, sc)])
-        visited[sr][sc] = 1
-        while Q:
-            cr, cc = Q.popleft()
-            for d in range(4):
-                nr, nc = cr+dr[d], cc+dc[d]
-                if 0 <= nr < N and 0 <= nc < M and visited[nr][nc] == 0 and arr[nr][nc] > 0:
-                    visited[nr][nc] = 1
-                    Q.append((nr, nc))
-    
-    for r in range(N):
-        for c in range(M):
-            if arr[r][c] > 0 and visited[r][c] == 0:
-                components += 1
-                if components > 1:
-                    return True
-                bfs(r, c)
-
-    return False
 
 def simulate():
-    global result
-    while True:
-        melting()
-        result += 1
+    years = 0
+    ice_cnt = sum(sum(row) > 0 for row in arr)
 
-        if check_split():
-            return result
+    while ice_cnt > 0:
+        ice_cnt = melting()  # 빙산 녹이기 및 개수 갱신
+        years += 1
 
-        # 맵 다 도는 동안 값이 싹다 0이면 0 리턴
-        if all(arr[r][c] == 0 for r in range(N) for c in range(M)):
-            return 0
+        if check_split(ice_cnt):
+            return years
+    return 0
+
 
 print(simulate())
