97a5b0d00837d3abd78d9d6822ccd295b9299c17
ooyejinn <156629458+ooyejinn@users.noreply.github.com>
2025-02-18 16:23:07 +0900

    [level 2] Title: 피로도, Time: 57.38 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -3,25 +3,15 @@ from itertools import permutations
 def solution(k, dungeons):
     answer = 0
     
-    for cave in permutations(dungeons):
-        now_k = k
+    for perm in permutations(dungeons):
+        perm_k = k
         cnt = 0
         
-        for required, cost in cave:
-            if now_k >= required:
-                now_k -= cost
+        for i in perm:
+            if perm_k >= i[0]:
+                perm_k -= i[1]
                 cnt += 1
-            else:
-                break
-        
-        answer = max(answer, cnt)
+                
+        answer = max(cnt, answer)
     
     return answer
-
-# permutations([[a,b], [c,d], [e,f]])
-# ([a,b], [c,d], [e,f])
-# ([a,b], [e,f], [c,d])
-# ([c,d], [a,b], [e,f])
-# ([c,d], [e,f], [a,b])
-# ([e,f], [a,b], [c,d])
-# ([e,f], [c,d], [a,b])
