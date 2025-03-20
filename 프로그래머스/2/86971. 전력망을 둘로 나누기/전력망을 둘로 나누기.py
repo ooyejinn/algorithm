203cda19ebecf32d158df51915ab67b97121f615
ooyejinn <156629458+ooyejinn@users.noreply.github.com>
2025-03-20 21:36:54 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 5.56 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,35 +1,33 @@
-# 노드가 N개고, 간선이 N-1개이면서, 모든 노드가 연결되어 있음
-# = 사이클이 없음
 from collections import deque
 
 def solution(n, wires):
-    arr = [[] for _ in range(n + 1)]
     answer = float('inf')
+    arr = [[] for _ in range(n+1)]
     
-    # 유용한 문법
-    for v1, v2 in wires:
-        arr[v1].append(v2)
-        arr[v2].append(v1)
+    for w1, w2 in wires:
+        arr[w1].append(w2)
+        arr[w2].append(w1)
         
-    def bfs(s, cut_v):
-        Q = deque([s])
+    def bfs(s, cut1, cut2):
+        Q = deque()
+        Q.append(s)
         visited = set([s])
         
         while Q:
             c = Q.popleft()
             
-            for new in arr[c]:
-                if (new, c) == cut_v or (c, new) == cut_v:
+            for next in arr[c]:
+                if (cut1 == c and cut2 == next) or (cut2 == c and cut1 == next):
                     continue
-            
-                if new not in visited:
-                    visited.add(new)
-                    Q.append(new)
                 
+                if next not in visited:
+                    visited.add(next)
+                    Q.append(next)
+                    
         return len(visited)
-    
-    for v1, v2 in wires:
-        cnt_subtree = bfs(v1, (v1, v2))
+        
+    for w1, w2 in wires:
+        cnt_subtree = bfs(w1, w1, w2)
         dif = abs(cnt_subtree - (n - cnt_subtree))
         answer = min(dif, answer)
     
