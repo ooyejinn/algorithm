8636f556ffd674d65c9cb79bc29d0a468147a4cb
ooyejinn <156629458+ooyejinn@users.noreply.github.com>
2025-03-21 14:18:47 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.44 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,30 +1,28 @@
 from collections import deque
 
 def solution(maps):
-    answer = 0
-    n = len(maps)
-    m = len(maps[0])
     dr = [1, -1, 0, 0]
     dc = [0, 0, 1, -1]
-    visited = [[0]*m for _ in range(n)]
+    N = len(maps)
+    M = len(maps[0])
+    visited = [[0]*M for _ in range(N)]
     
-    def bfs():
-        Q = deque()
-        Q.append((0, 0, 1))
-        visited[0][0] = 1
-
-        while Q:
-            r, c, cnt = Q.popleft()
-
-            if r == n-1 and c == m-1:
-                return cnt
-
-            for d in range(4):
-                nr, nc = r+dr[d], c+dc[d]
-                if 0 <= nr < n and 0 <= nc < m and visited[nr][nc] == 0 and maps[nr][nc] == 1:
-                    visited[nr][nc] =1
-                    Q.append((nr, nc, cnt+1))
-                    
-        return -1
+    Q = deque()
+    Q.append((0, 0, 1))
+    visited[0][0] = 1
     
-    return bfs()
+    while Q:
+        cr, cc, ccnt = Q.popleft()
+        
+        if cr == N-1 and cc == M-1:
+            return ccnt
+        
+        for d in range(4):
+            nr = cr + dr[d]
+            nc = cc + dc[d]
+            
+            if 0 <= nr < N and 0 <= nc < M and not visited[nr][nc] and maps[nr][nc] == 1:
+                visited[nr][nc] = 1
+                Q.append((nr, nc, ccnt+1))
+    
+    return -1
