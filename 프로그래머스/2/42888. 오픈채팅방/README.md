5afcd5de7083bc29f688b6c8cc8d812ea8721b0a
ooyejinn <156629458+ooyejinn@users.noreply.github.com>
2025-06-11 16:39:29 +0900

    [level 2] Title: 오픈채팅방, Time: 285.12 ms, Memory: 99 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/README.md
+++ 프로그래머스/2/42888. 오픈채팅방/README.md
@@ -1,10 +1,10 @@
 # [level 2] 오픈채팅방 - 42888 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42888?language=javascript) 
 
 ### 성능 요약
 
-메모리: 40.5 MB, 시간: 100.57 ms
+메모리: 99 MB, 시간: 285.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 11일 16:22:52
+2025년 06월 11일 16:39:27
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -0,0 +1,28 @@
+function solution(record) {
+    const answer = [];
+    const user_nick = {};
+    
+    for (let i = 0; i < record.length; i++) {
+        const parts = record[i].split(" ");
+        const cmd = parts[0];
+        const uid = parts[1];
+        
+        if (cmd === "Enter" || cmd === "Change") {
+            user_nick[uid] = parts[2];
+        }
+    }
+    
+    for (let i = 0; i < record.length; i++) {
+        const parts = record[i].split(" ");
+        const cmd = parts[0];
+        const uid = parts[1];
+        
+        if (cmd === "Enter") {
+            answer.push(`${user_nick[uid]}님이 들어왔습니다.`);
+        } else if (cmd === "Leave") {
+            answer.push(`${user_nick[uid]}님이 나갔습니다.`);
+        }
+    }
+    
+    return answer;
+}
