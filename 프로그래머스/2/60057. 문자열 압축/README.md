05b6ca3391bf21966806283631414db34a28b687
ooyejinn <156629458+ooyejinn@users.noreply.github.com>
2025-06-24 18:10:32 +0900

    [level 2] Title: 문자열 압축, Time: 2.04 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/README.md
+++ 프로그래머스/2/60057. 문자열 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] 문자열 압축 - 60057 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057?language=python3) 
 
 ### 성능 요약
 
-메모리: 37 MB, 시간: 32.92 ms
+메모리: 9.27 MB, 시간: 2.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 12일 18:10:46
+2025년 06월 24일 18:10:30
 
 ### 문제 설명
 
--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -2,24 +2,28 @@ def solution(s):
     n = len(s)
     answer = n
     
-    for unit_num in range(1, n // 2 + 1):
+    for unit_num in range(1, n//2+1):
         result = ""
         prev_str = s[0:unit_num]
         cnt = 1
         
-        # 0부터 n까지 cut_num 씩 증가
         for i in range(unit_num, n, unit_num):
-            # 직전것과 똑같이 반복될 경우
-            if s[i:i+unit_num] == prev_str:
+            now_str = s[i:i+unit_num]
+            if prev_str == now_str:
                 cnt += 1
-            
             else:
-                result += str(cnt) + prev_str if cnt > 1 else prev_str
-                prev_str = s[i:i+unit_num]
+                if cnt == 1:
+                    result += prev_str
+                else:
+                    result += (str(cnt) + prev_str)
+                prev_str = now_str
                 cnt = 1
-                
-        result += str(cnt) + prev_str if cnt > 1 else prev_str
+
+        if cnt == 1:
+            result += prev_str
+        else:
+            result += (str(cnt) + prev_str)
         
         answer = min(answer, len(result))
-    
+                    
     return answer
