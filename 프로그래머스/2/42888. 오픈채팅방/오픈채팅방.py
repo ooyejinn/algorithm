4a950db7339e206b051cf7af3d99b062cf0f0b28
ooyejinn <156629458+ooyejinn@users.noreply.github.com>
2025-06-24 17:25:39 +0900

    [level 2] Title: 오픈채팅방, Time: 114.34 ms, Memory: 40.6 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,24 +1,22 @@
 def solution(record):
-    result = []
+    answer = []
     user_nick = {}
     
     for r in record:
         parts = r.split()
-        cmd = parts[0]
+        active = parts[0]
         uid = parts[1]
         
-        if cmd == "Enter" or cmd == "Change":
-            nick = parts[2]
-            user_nick[uid] = nick
+        if active == "Enter" or active == "Change":
+            user_nick[uid] = parts[2]
     
     for r in record:
         parts = r.split()
-        cmd = parts[0]
+        active = parts[0]
         uid = parts[1]
-        
-        if cmd == "Enter":
-            result.append(f"{user_nick[uid]}님이 들어왔습니다.")
-        elif cmd == "Leave":
-            result.append(f"{user_nick[uid]}님이 나갔습니다.")
+        if active == "Enter":
+            answer.append(f"{user_nick[uid]}님이 들어왔습니다.")
+        elif active == "Leave":
+            answer.append(f"{user_nick[uid]}님이 나갔습니다.")
     
-    return result
+    return answer
