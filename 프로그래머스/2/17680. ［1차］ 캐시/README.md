1058bda4c0301d1c771cb7827f33f4d700d8a232
ooyejinn <156629458+ooyejinn@users.noreply.github.com>
2025-08-04 18:02:02 +0900

    [level 2] Title: [1차] 캐시, Time: 85.05 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/README.md
+++ 프로그래머스/2/17680. ［1차］ 캐시/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 캐시 - 17680 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680?language=python3) 
 
 ### 성능 요약
 
-메모리: 41.9 MB, 시간: 49.32 ms
+메모리: 16.6 MB, 시간: 85.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 19일 17:10:24
+2025년 08월 04일 18:01:58
 
 ### 문제 설명
 
--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,28 +1,27 @@
 from collections import deque
+# deque 의 popleft: 선입선출
 
 def solution(cacheSize, cities):
     N = len(cities)
     answer = 0
     
     if cacheSize == 0:
-        return N*5
+        return N * 5
     
     Q = deque()
     
     for i in range(N):
         city = cities[i].lower()
         
-        # 만약 deque에 없다면
         if city not in Q:
             answer += 5
-            
+        
             if len(Q) < cacheSize:
                 Q.append(city)
             else:
                 Q.popleft()
                 Q.append(city)
-        
-        # 만약 deque에 있다면
+                
         else:
             answer += 1
             Q.remove(city)
