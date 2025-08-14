789b81a13a0691098508e0ebb7f7d0662fb2f98d
ooyejinn <156629458+ooyejinn@users.noreply.github.com>
2025-08-14 17:37:12 +0900

    [Gold II] Title: 문제 추천 시스템 Version 2, Time: 604 ms, Memory: 147656 KB -BaekjoonHub

--- 백준/Gold/21944. 문제 추천 시스템 Version 2/문제 추천 시스템 Version 2.py
+++ 백준/Gold/21944. 문제 추천 시스템 Version 2/문제 추천 시스템 Version 2.py
@@ -2,23 +2,19 @@ from collections import defaultdict
 
 N = int(input())
 
-# L: (P1, P2 ...)
-levels = defaultdict(set)
-# P1: (L), P2: (L) ...
-problem_l = {}
-# P1: (G), P2: (G) ...
-problem_g = {}
-# G: (L1: (P1, P2 ...), L2: (P3, P4 ...))
-g_levels = defaultdict(lambda: defaultdict(set))
-
-max_l = 100
+# L: (P1, P2...)
+LP_graph = defaultdict(set)
+# G: {L1: P1, P2..., L2: P3, P4...}
+GLP_graph = defaultdict(lambda: defaultdict(set))
+# P: [L, G]
+P_graph = {}
+# -> solved 처리에 역방향 맵이 있으면 좋다
 
 for _ in range(N):
     P, L, G = map(int, input().split())
-    levels[L].add(P)
-    problem_l[P] = L
-    problem_g[P] = G
-    g_levels[G][L].add(P)
+    LP_graph[L].add(P)
+    GLP_graph[G][L].add(P)
+    P_graph[P] = [L, G]
 
 M = int(input())
 
@@ -26,81 +22,64 @@ for _ in range(M):
     act = input().split()
 
     if act[0] == "add":
-        P = int(act[1])
-        L = int(act[2])
-        G = int(act[3])
+        P, L, G = int(act[1]), int(act[2]), int(act[3])
+        LP_graph[L].add(P)
+        GLP_graph[G][L].add(P)
+        P_graph[P] = [L, G]
 
-        levels[L].add(P)
-        problem_l[P] = L
-        problem_g[P] = G
-        g_levels[G][L].add(P)
+    elif act[0] == "solved":
+        P = int(act[1])
+
+        if P in P_graph:
+            L, G = P_graph[P][0], P_graph[P][1]
+            LP_graph[L].discard(P)
+            GLP_graph[G][L].discard(P)
+            del P_graph[P]
 
     elif act[0] == "recommend":
-        G = int(act[1])
-        X = int(act[2])
+        G, X = int(act[1]), int(act[2])
 
         if X == 1:
-            # 내림차순
-            for lv in range(max_l, 0, -1):
-                if g_levels[G][lv]:
-                    print(max(g_levels[G][lv]))
+            for lv in range(100, 0, -1):
+                if GLP_graph[G][lv]:
+                    print(max(GLP_graph[G][lv]))
                     break
-
         else:
-            # 오름차순
-            for lv in range(1, max_l+1):
-                if g_levels[G][lv]:
-                    print(min(g_levels[G][lv]))
+            for lv in range(1, 101):
+                if GLP_graph[G][lv]:
+                    print(min(GLP_graph[G][lv]))
                     break
 
     elif act[0] == "recommend2":
         X = int(act[1])
 
         if X == 1:
-            for lv in range(max_l, 0, -1):
-                if levels[lv]:
-                    print(max(levels[lv]))
+            for lv in range(100, 0, -1):
+                if LP_graph[lv]:
+                    print(max(LP_graph[lv]))
                     break
-
         else:
-            for lv in range(1, max_l+1):
-                if levels[lv]:
-                    print(min(levels[lv]))
+            for lv in range(1, 101):
+                if LP_graph[lv]:
+                    print(min(LP_graph[lv]))
                     break
 
     elif act[0] == "recommend3":
-        X = int(act[1])
-        L = int(act[2])
+        X, L = int(act[1]), int(act[2])
+        flag = False
 
         if X == 1:
-            flag = False
-            for lv in range(L, max_l+1):
-                if levels[lv]:
-                    print(min((levels[lv])))
+            for lv in range(L, 101):
+                if LP_graph[lv]:
+                    print(min(LP_graph[lv]))
                     flag = True
                     break
-            if not flag:
-                print(-1)
-
         else:
-            flag = False
-            for lv in range(min(L-1, max_l), 0, -1):
-                if levels[lv]:
-                    print(max(levels[lv]))
+            for lv in range(L-1, 0, -1):
+                if LP_graph[lv]:
+                    print(max(LP_graph[lv]))
                     flag = True
                     break
-            if not flag:
-                print(-1)
 
-    elif act[0] == "solved":
-        P = int(act[1])
-        L = problem_l.pop(P, None)
-        G = problem_g.pop(P, None)
-
-        # discard():
-        # set 메서드, 특정 원소가 있을 시 삭제, 없을 시 무시
-        # 반환값: None
-        if L is not None:
-            levels[L].discard(P)
-        if G is not None:
-            g_levels[G][L].discard(P)
+        if not flag:
+            print(-1)
