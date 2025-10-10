c54a5485bdc3e7637eaed566c816bd040b6bae52
ooyejinn <156629458+ooyejinn@users.noreply.github.com>
2025-10-10 17:12:43 +0900

    [level 3] Title: 베스트앨범, Time: 0.13 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,30 +1,38 @@
 from collections import defaultdict
 
 def solution(genres, plays):
+    # 장르 별 두개씩
+    # 1. 장르 합산 높은 장르부터 (모든 장르는 횟수 다름)
+    # 2. 장르 내에서 더 많이 재생된 노래
+    # 3. 장르 내 횟수 같을 시 고유번호 낮은 노래
+    
     N = len(plays)
     genres_cnt = defaultdict(int)
+    # {'pop': [(재생횟수, 고유번호), ()]}
     
     for i in range(N):
-        genre = genres[i]
-        play = plays[i]
-        genres_cnt[genre] += play
+        g = genres[i]
+        p = plays[i]
+        
+        genres_cnt[g] += p
         
     song_info = []
     
     for i in range(N):
-        genre = genres[i]
-        play = plays[i]
+        g = genres[i]
+        p = plays[i]
         
-        song_info.append([-genres_cnt[genre], -play, i, genre])
+        #                 -genres_cnt: 이렇게 하면 자동으로 높은 순서부터
+        song_info.append([-genres_cnt[g], -p, i, g])
         
     song_info.sort()
     
-    genre_picked = defaultdict(int)
+    genre_picked = defaultdict(int)    
     answer = []
     
-    for _, _, i, genre in song_info:
-        if genre_picked[genre] < 2:
+    for _, _, i, g in song_info:
+        if genre_picked[g] < 2:
             answer.append(i)
-            genre_picked[genre] += 1
-            
+            genre_picked[g] += 1
+    
     return answer
